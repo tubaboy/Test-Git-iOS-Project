ecb2a9943e31f744cfb507b63f030502885d949f
David Chuang <DavidChuang@Davids-MacBook-Air.local>
2014-03-20 15:15:14 +0800

    added a test class

---
+++ Git Test/DCTest.m
@@ -0,0 +1,13 @@
+//
+//  DCTest.m
+//  Git Test
+//
+//  Created by David Chuang on 3/20/14.
+//  Copyright (c) 2014 David Chuang. All rights reserved.
+//
+
+#import "DCTest.h"
+
+@implementation DCTest
+
+@end
